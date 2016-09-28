6ce1b6d51ed7895e12b5cd5a30401b6114ceb31f
Naoki Sugimoto <assoonasp_t@yahoo.co.jp>
2016-09-29 01:49:55 +0900

    initial commit

---
+++ ArduinoPiano/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace ArduinoPiano
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// serialPort1
+			// 
+			this.serialPort1.PortName = "COM3";
+			this.serialPort1.RtsEnable = true;
+			// 
+			// button1
+			// 
+			this.button1.AutoSize = true;
+			this.button1.Location = new System.Drawing.Point(37, 47);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "ド";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.AutoSize = true;
+			this.button2.Location = new System.Drawing.Point(118, 47);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 0;
+			this.button2.Text = "レ";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click_1);
+			// 
+			// button3
+			// 
+			this.button3.AutoSize = true;
+			this.button3.Location = new System.Drawing.Point(199, 47);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 0;
+			this.button3.Text = "ミ";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click_1);
+			// 
+			// button4
+			// 
+			this.button4.AutoSize = true;
+			this.button4.Location = new System.Drawing.Point(280, 47);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 0;
+			this.button4.Text = "ファ";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click_1);
+			// 
+			// button5
+			// 
+			this.button5.AutoSize = true;
+			this.button5.Location = new System.Drawing.Point(361, 47);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 0;
+			this.button5.Text = "ソ";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click_1);
+			// 
+			// button6
+			// 
+			this.button6.AutoSize = true;
+			this.button6.Location = new System.Drawing.Point(442, 47);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(75, 23);
+			this.button6.TabIndex = 0;
+			this.button6.Text = "ラ";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button7
+			// 
+			this.button7.AutoSize = true;
+			this.button7.Location = new System.Drawing.Point(523, 47);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(75, 23);
+			this.button7.TabIndex = 0;
+			this.button7.Text = "シ";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click_1);
+			// 
+			// button8
+			// 
+			this.button8.AutoSize = true;
+			this.button8.Location = new System.Drawing.Point(604, 47);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(75, 23);
+			this.button8.TabIndex = 0;
+			this.button8.Text = "ド";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click_1);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(37, 116);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(645, 96);
+			this.label1.TabIndex = 1;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(720, 235);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Arduino Piano";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ ArduinoPiano/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ArduinoPiano
+{
+	public partial class Form1 : Form
+	{
+		SerialPort serialPort1;
+		public Form1()
+		{
+			InitializeComponent();
+			KeyPreview = true;
+			serialPort1 = new System.IO.Ports.SerialPort("COM3", 9600);
+			serialPort1.Open();
+		}
+
+		protected override void OnKeyDown(KeyEventArgs e)
+		{
+			base.OnKeyDown(e);
+			if (e.KeyData == Keys.Escape)
+			{
+				label1.Text = "";
+			}
+		}
+		
+		private void button1_Click(object sender, EventArgs e)
+		{
+			serialPort1.Write("C");
+			label1.Text += "ド";
+		}
+
+		private void button2_Click_1(object sender, EventArgs e)
+		{
+			serialPort1.Write("D");
+			label1.Text += "レ";
+		}
+
+		private void button3_Click_1(object sender, EventArgs e)
+		{
+			serialPort1.Write("E");
+			label1.Text += "ミ";
+		}
+
+		private void button4_Click_1(object sender, EventArgs e)
+		{
+			serialPort1.Write("F");
+			label1.Text += "ファ";
+		}
+
+		private void button5_Click_1(object sender, EventArgs e)
+		{
+			serialPort1.Write("G");
+			label1.Text += "ソ";
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			serialPort1.Write("A");
+			label1.Text += "ラ";
+		}
+
+		private void button7_Click_1(object sender, EventArgs e)	//シ
+		{
+			serialPort1.Write("B");
+			label1.Text += "シ";
+		}
+
+		private void button8_Click_1(object sender, EventArgs e)
+		{
+			serialPort1.Write("c");
+			label1.Text += "ﾄﾞ";
+		}
+	}
+}
